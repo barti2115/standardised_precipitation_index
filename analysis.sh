f6883c86dcad721eda6563c5dfb9bab2aca7e628
Jakub Grabski <kuba.grabski1@gmail.com>
2024-04-24 17:11:16 +0200

    One step at a time... ~ NASA

---
+++ analysis.sh
@@ -0,0 +1,8 @@
+python3 EDA.py
+echo "Stage EDA completed!"
+
+python3 SPI_calculation.py
+echo "Stage SPI calculation completed!"
+
+python3 SPI_visualization.py
+echo "Stage SPI Visualization completed!"
